f5f277034e2851ba4f10f5f6904f8b29d51df204
hpriim2gi <henri.priim2gi@hotmail.com>
2023-11-24 18:59:19 +0200

    update

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const db = require('../data/database');
+const mongodb = require('mongodb');
+
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -15,6 +18,24 @@ router.get('/new-post', async function(req, res) {
   const authors = await db.getDb().collection('authors').find().toArray();         //getting the collection of authors, returns a promise
   
   res.render('create-post', {authors: authors});
+});
+
+
+router.get('/posts', async function(req, res){
+  const authorId = new ObjectId(req.body.author);
+  const author = await db.getDb().collection('authors').findOne({_id: authorId});
+  
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    }
+  };
 });
 
 module.exports = router;
